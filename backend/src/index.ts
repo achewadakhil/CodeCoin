83ea7ee439df994364fea9e6d98d14ecfddbb44d
Akhil <achewadakhil11@gmail.com>
2025-08-14 16:47:47 +0530

    Initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+
+const app = express();
+
+app.use(express.json());
+app.post("/home",(req,res)=>{
+  res.json({
+    message : "Hello, your email is "
+  });
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
